386daa19ca518c9eebc56723d8c7834e0225ba28
tsbraun1891 <tsbraun1891@gmail.com>
2022-05-05 11:54:10 -0400

    Add optional argument for blank output

--- fakeish/objects/string/__init__.py
+++ fakeish/objects/string/__init__.py
@@ -5,5 +5,7 @@ import string
 class RandomObject:
 
     @staticmethod
-    def rand_string(s: [int, None] = 512):
+    def rand_string(s: [int, None] = 512, blank: float = 0):
+        if blank > 0 and random.uniform(0, 1) < blank:
+            return ""
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(s))
